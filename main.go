4ad2dd6418fa2aba5af0eaa30068a5ef8bd3f013
sanjeev40 <sanjeevraichur29@gmail.com>
2025-03-20 02:32:37 -0400

    add basic shapes

--- main.go
+++ main.go
@@ -1,8 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"image/color"
 
-func main() {
-    fmt.Println("Hello world!")
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type Paddle struct {
+	Width  int
+	Height int
+	Color  color.RGBA
 }
 
+type Ball struct {
+	Radius float32
+	Color  color.RGBA
+}
+
+func main() {
+	var screenWidth, screenHeight int32 = 800, 450
+
+	rl.InitWindow(screenWidth, screenHeight, "Pong")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	// Set paddle config
+	paddle := Paddle{Width: 10, Height: 80, Color: rl.Black}
+	ball := Ball{Radius: 7.5, Color: rl.Red}
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		rl.DrawRectangleV(
+			rl.Vector2{
+				X: 5.0,
+				Y: float32(screenHeight)/2.0 - float32(paddle.Height)/2.0,
+			},
+			rl.Vector2{
+				X: float32(paddle.Width),
+				Y: float32(paddle.Height),
+			},
+			paddle.Color,
+		)
+
+		rl.DrawRectangleV(
+			rl.Vector2{
+				X: float32(screenWidth) - float32(paddle.Width) - 5.0,
+				Y: float32(screenHeight)/2.0 - float32(paddle.Height)/2.0,
+			},
+			rl.Vector2{
+				X: float32(paddle.Width),
+				Y: float32(paddle.Height),
+			},
+			paddle.Color,
+		)
+
+		rl.DrawCircleV(
+			rl.Vector2{
+				X: float32(screenWidth)/2.0 - ball.Radius,
+				Y: float32(screenHeight)/2.0 - ball.Radius,
+			},
+			ball.Radius,
+			ball.Color,
+		)
+
+		rl.EndDrawing()
+	}
+}
