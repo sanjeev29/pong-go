02badd96a5eb55e16feb878cc68d27bd352a46c9
sanjeev40 <sanjeevraichur29@gmail.com>
2025-03-21 16:45:50 -0400

    bounce ball off left paddle

--- main.go
+++ main.go
@@ -6,9 +6,17 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+// TODO: MOVE TO CONSTANTS FILE?
 var PLAYER_VERTICAL_SPEED float32 = 200.0
+var PLAYER_OFFSET float32 = 5.0
+
 var PADDLE_HEIGHT float32 = 80.0
 var PADDLE_WIDTH float32 = 10.0
+
+var BALL_SPEED_X float32 = 4.0
+var BALL_SPEED_Y float32 = 5.0
+var BALL_RADIUS float32 = 7.5
+var BALL_COLOR color.RGBA = rl.Red
 
 type Paddle struct {
 	Width  float32
@@ -17,8 +25,10 @@ type Paddle struct {
 }
 
 type Ball struct {
-	Radius float32
-	Color  color.RGBA
+	Speed    rl.Vector2
+	Position rl.Vector2
+	Radius   float32
+	Color    color.RGBA
 }
 
 type Player struct {
@@ -47,6 +57,36 @@ func (p *Player) Update(deltaTime float32) {
 	}
 }
 
+func (b *Ball) Update() {
+	b.Position.X += b.Speed.X
+	b.Position.Y += b.Speed.Y
+
+	screenHeight := float32(rl.GetScreenHeight())
+
+	// Ball must bounce off of top and bottom walls
+	if b.Position.Y >= screenHeight-BALL_RADIUS ||
+		b.Position.Y <= BALL_RADIUS {
+		b.Speed.Y *= -1.0
+	}
+}
+
+func checkCollision(playerA *Player, ball *Ball) {
+	screenWidth := float32(rl.GetScreenWidth())
+	// screenHeight := float32(rl.GetScreenHeight())
+
+	// Check collision with player A (left paddle))
+	if ball.Position.X <= playerA.Position.X+PADDLE_WIDTH+PLAYER_OFFSET+BALL_RADIUS &&
+		ball.Position.Y >= playerA.Position.Y &&
+		ball.Position.Y <= playerA.Position.Y+PADDLE_HEIGHT {
+		ball.Speed.X *= -1.0
+	}
+
+	if ball.Position.X >= screenWidth-BALL_RADIUS ||
+		ball.Position.X <= BALL_RADIUS {
+		ball.Speed.X *= -1.0
+	}
+}
+
 func main() {
 	var screenWidth, screenHeight int32 = 800, 450
 
@@ -61,7 +101,19 @@ func main() {
 		Height: PADDLE_HEIGHT,
 		Color:  rl.Black,
 	}
-	ball := Ball{Radius: 7.5, Color: rl.Red}
+
+	// Set ball config
+	ball := Ball{}
+	ball.Speed = rl.Vector2{
+		X: BALL_SPEED_X,
+		Y: BALL_SPEED_Y,
+	}
+	ball.Position = rl.Vector2{
+		X: float32(screenWidth/2.0) - BALL_RADIUS,
+		Y: float32(screenHeight/2.0) - BALL_RADIUS,
+	}
+	ball.Radius = BALL_RADIUS
+	ball.Color = BALL_COLOR
 
 	// Setup player A
 	// Default position is at the center of the screen
@@ -69,15 +121,23 @@ func main() {
 	// Default speed = 0
 	playerA := Player{}
 	playerA.Position = rl.Vector2{
-		X: 5.0,
+		X: PLAYER_OFFSET,
 		Y: float32(screenHeight)/2.0 - float32(paddle.Height)/2.0,
 	}
 
 	for !rl.WindowShouldClose() {
 		deltaTime := rl.GetFrameTime()
 
+		// Update player position
 		playerA.Update(deltaTime)
 
+		// Update ball position
+		ball.Update()
+
+		// Check ball and player collision
+		checkCollision(&playerA, &ball)
+
+		// BEGIN DRAWING
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
@@ -104,10 +164,7 @@ func main() {
 		)
 
 		rl.DrawCircleV(
-			rl.Vector2{
-				X: float32(screenWidth)/2.0 - ball.Radius,
-				Y: float32(screenHeight)/2.0 - ball.Radius,
-			},
+			ball.Position,
 			ball.Radius,
 			ball.Color,
 		)
