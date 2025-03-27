69c8c3d64952d8de75e2c398d75aa122d45bdbda
sanjeev40 <sanjeevraichur29@gmail.com>
2025-03-27 00:14:46 -0400

    add bot paddle movement

--- main.go
+++ main.go
@@ -1,174 +1,140 @@
 package main
 
 import (
-	"image/color"
-
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-// TODO: MOVE TO CONSTANTS FILE?
-var PLAYER_VERTICAL_SPEED float32 = 200.0
-var PLAYER_OFFSET float32 = 5.0
+const (
+	screenWidth  = 800.0
+	screenHeight = 600.0
+	paddleWidth  = 10.0
+	paddleHeight = 80.0
+	paddleOffset = 5.0
+	paddleSpeed  = 5.0
+	ballRadius   = 7.5
+	ballSpeed    = 5.0
+)
 
-var PADDLE_HEIGHT float32 = 80.0
-var PADDLE_WIDTH float32 = 10.0
-
-var BALL_SPEED_X float32 = 4.0
-var BALL_SPEED_Y float32 = 5.0
-var BALL_RADIUS float32 = 7.5
-var BALL_COLOR color.RGBA = rl.Red
-
-type Paddle struct {
-	Width  float32
-	Height float32
-	Color  color.RGBA
-}
-
-type Ball struct {
-	Speed    rl.Vector2
-	Position rl.Vector2
-	Radius   float32
-	Color    color.RGBA
-}
-
-type Player struct {
-	Position rl.Vector2
-	Speed    int
-}
-
-func (p *Player) Update(deltaTime float32) {
-	// If UP_ARROW is pressed, move player up
-	if rl.IsKeyDown(rl.KeyUp) {
-		p.Position.Y -= (PLAYER_VERTICAL_SPEED * deltaTime)
+var (
+	// Right side
+	playerPaddle = rl.Rectangle{
+		X:      screenWidth - paddleWidth - paddleOffset,
+		Y:      screenHeight/2.0 - paddleHeight/2.0,
+		Width:  paddleWidth,
+		Height: paddleHeight,
 	}
 
-	// If DOWN_ARROW is pressed, move player down
-	if rl.IsKeyDown(rl.KeyDown) {
-		p.Position.Y += (PLAYER_VERTICAL_SPEED * deltaTime)
+	// Left side
+	botPaddle = rl.Rectangle{
+		X:      paddleOffset,
+		Y:      screenHeight/2.0 - paddleHeight/2.0,
+		Width:  paddleWidth,
+		Height: paddleHeight,
 	}
 
-	// Keep paddle movement boundaries within screen height
-	screenHeight := float32(rl.GetScreenHeight())
-	if p.Position.Y < 1 {
-		p.Position.Y = 1
+	ball = rl.Vector2{
+		X: screenWidth/2.0 - ballRadius/2.0,
+		Y: screenHeight/2.0 - ballRadius/2.0,
 	}
-	if p.Position.Y > screenHeight-PADDLE_HEIGHT-1 {
-		p.Position.Y = screenHeight - PADDLE_HEIGHT - 1
+	ballVelocity = rl.Vector2{
+		X: ballSpeed,
+		Y: ballSpeed,
 	}
-}
-
-func (b *Ball) Update() {
-	b.Position.X += b.Speed.X
-	b.Position.Y += b.Speed.Y
-
-	screenHeight := float32(rl.GetScreenHeight())
-
-	// Ball must bounce off of top and bottom walls
-	if b.Position.Y >= screenHeight-BALL_RADIUS ||
-		b.Position.Y <= BALL_RADIUS {
-		b.Speed.Y *= -1.0
-	}
-}
-
-func checkCollision(playerA *Player, ball *Ball) {
-	screenWidth := float32(rl.GetScreenWidth())
-	// screenHeight := float32(rl.GetScreenHeight())
-
-	// Check collision with player A (left paddle))
-	if ball.Position.X <= playerA.Position.X+PADDLE_WIDTH+PLAYER_OFFSET+BALL_RADIUS &&
-		ball.Position.Y >= playerA.Position.Y &&
-		ball.Position.Y <= playerA.Position.Y+PADDLE_HEIGHT {
-		ball.Speed.X *= -1.0
-	}
-
-	if ball.Position.X >= screenWidth-BALL_RADIUS ||
-		ball.Position.X <= BALL_RADIUS {
-		ball.Speed.X *= -1.0
-	}
-}
+)
 
 func main() {
-	var screenWidth, screenHeight int32 = 800, 450
-
-	rl.InitWindow(screenWidth, screenHeight, "Pong")
+	rl.InitWindow(int32(screenWidth), int32(screenHeight), "Pong")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
 
-	// Set paddle config
-	paddle := Paddle{
-		Width:  PADDLE_WIDTH,
-		Height: PADDLE_HEIGHT,
-		Color:  rl.Black,
-	}
-
-	// Set ball config
-	ball := Ball{}
-	ball.Speed = rl.Vector2{
-		X: BALL_SPEED_X,
-		Y: BALL_SPEED_Y,
-	}
-	ball.Position = rl.Vector2{
-		X: float32(screenWidth/2.0) - BALL_RADIUS,
-		Y: float32(screenHeight/2.0) - BALL_RADIUS,
-	}
-	ball.Radius = BALL_RADIUS
-	ball.Color = BALL_COLOR
-
-	// Setup player A
-	// Default position is at the center of the screen
-	// on the player side
-	// Default speed = 0
-	playerA := Player{}
-	playerA.Position = rl.Vector2{
-		X: PLAYER_OFFSET,
-		Y: float32(screenHeight)/2.0 - float32(paddle.Height)/2.0,
-	}
-
 	for !rl.WindowShouldClose() {
-		deltaTime := rl.GetFrameTime()
+		// Updates game elements
+		updateGame()
 
-		// Update player position
-		playerA.Update(deltaTime)
+		// Draw game elements
+		drawGame()
+	}
+}
 
-		// Update ball position
-		ball.Update()
+func updateGame() {
+	updateBall()
 
-		// Check ball and player collision
-		checkCollision(&playerA, &ball)
+	updatePlayerPaddle()
 
-		// BEGIN DRAWING
-		rl.BeginDrawing()
+	updateBotPaddle()
+}
 
-		rl.ClearBackground(rl.RayWhite)
+func drawGame() {
+	rl.BeginDrawing()
+	rl.ClearBackground(rl.Black)
 
-		rl.DrawRectangleV(
-			playerA.Position,
-			rl.Vector2{
-				X: paddle.Width,
-				Y: paddle.Height,
-			},
-			paddle.Color,
-		)
+	// Draw playerPaddle
+	rl.DrawRectangleRec(playerPaddle, rl.Blue)
 
-		rl.DrawRectangleV(
-			rl.Vector2{
-				X: float32(screenWidth) - float32(paddle.Width) - 5.0,
-				Y: float32(screenHeight)/2.0 - float32(paddle.Height)/2.0,
-			},
-			rl.Vector2{
-				X: paddle.Width,
-				Y: paddle.Height,
-			},
-			paddle.Color,
-		)
+	// Draw botPaddle
+	rl.DrawRectangleRec(botPaddle, rl.Green)
 
-		rl.DrawCircleV(
-			ball.Position,
-			ball.Radius,
-			ball.Color,
-		)
+	// Draw ball
+	rl.DrawCircle(int32(ball.X), int32(ball.Y), ballRadius, rl.Red)
 
-		rl.EndDrawing()
+	// Draw center line
+	rl.DrawLine(screenWidth/2.0, 0, screenWidth/2.0, screenHeight, rl.RayWhite)
+
+	rl.EndDrawing()
+
+}
+
+func updateBall() {
+	ball.X += ballVelocity.X
+	ball.Y += ballVelocity.Y
+
+	// Ball out of bounds
+	if ball.X <= 0 || ball.X >= screenWidth {
+		// Ball position and speed are reset
+		ball.X = screenWidth/2.0 - ballRadius/2.0
+		ball.Y = screenHeight/2.0 - ballRadius/2.0
+		ballVelocity.X *= -1.0
+	}
+
+	// Ball must bounce off of top and bottom walls
+	if ball.Y >= screenHeight-ballRadius ||
+		ball.Y <= ballRadius {
+		ballVelocity.Y *= -1.0
+	}
+
+	// Ball collision with player / bot paddle
+	if rl.CheckCollisionCircleRec(ball, ballRadius, playerPaddle) ||
+		rl.CheckCollisionCircleRec(ball, ballRadius, botPaddle) {
+		ballVelocity.X *= -1.0
+	}
+}
+
+func updatePlayerPaddle() {
+	// If UP_ARROW is pressed, move player up
+	if rl.IsKeyDown(rl.KeyUp) &&
+		playerPaddle.Y > 0 {
+		playerPaddle.Y -= (paddleSpeed)
+	}
+
+	// If DOWN_ARROW is pressed, move player down
+	if rl.IsKeyDown(rl.KeyDown) &&
+		playerPaddle.Y+paddleHeight < screenHeight {
+		playerPaddle.Y += (paddleSpeed)
+	}
+}
+
+// Simple bot movement based on ball.Y position
+func updateBotPaddle() {
+	if ball.Y > botPaddle.Y+(paddleHeight/2.0) {
+		botPaddle.Y += (paddleSpeed)
+	} else if ball.Y < botPaddle.Y+(paddleHeight/2.0) {
+		botPaddle.Y -= (paddleSpeed)
+	}
+
+	if botPaddle.Y < 0 {
+		botPaddle.Y = 0
+	} else if botPaddle.Y+paddleHeight > screenHeight {
+		botPaddle.Y = screenHeight - paddleHeight
 	}
 }
